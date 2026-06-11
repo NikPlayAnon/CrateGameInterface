45dd995eaf389f60473ee7b06e95df8a2a67a3d4
NikPlayAnon <nikolasatana16@gmail.com>
2026-06-11 23:44:23 +0300

    switched jpa for jdbc

--- src/main/kotlin/npa/crategame/crategameinterface/infrastructure/persistence/PlayerJpaRepository.kt
+++
@@ -1,8 +0,0 @@
-package npa.crategame.crategameinterface.infrastructure.persistence
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface PlayerJpaRepository : JpaRepository<PlayerEntity, UUID> {
-    fun existsByUsername(username: String): Boolean
-}
